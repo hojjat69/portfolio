f9405d56d5a8fb138667cb455e30db26b155db6a
Hojjat Borhany <hojjat.borhany@gmail.com>
2019-12-06 07:55:01 +0100

    jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
 # Create your models here.
+
+# It needs to have a title, pub date, body, image 
+
+# create a bolg model
+
+# add the blog app to the setting
+
+# create a migration 
+
+# migrate
+
+# add to the admin
+
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title =models.CharField(max_length=300)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    body = models.CharField(max_length=300)
+
+
+
+
